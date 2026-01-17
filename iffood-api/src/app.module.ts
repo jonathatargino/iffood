ffa8fd5e87d91e04f7327bde0df5129e3f83aea4
Jonatha Targino <j.oliveiratargino@hotmail.com>
2026-01-17 18:10:42 -0300

    refact: move validation pipe to nest providers to apply DI

--- iffood-api/src/app.module.ts
+++ iffood-api/src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { StoreModule } from './modules/store/store.module';
 import { ProductsModule } from './modules/product/product.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { LoggingInterceptor } from './common/interceptors/logging-interceptor';
 
 @Module({
@@ -11,6 +11,15 @@ import { LoggingInterceptor } from './common/interceptors/logging-interceptor';
     {
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor,
+    },
+    {
+      provide: APP_PIPE,
+      useFactory: () => {
+        return new ValidationPipe({
+          whitelist: true,
+          transform: true,
+        });
+      },
     },
   ],
   imports: [
