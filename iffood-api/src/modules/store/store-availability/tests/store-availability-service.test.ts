b54746f673eb4910be8e3f0217b183206e775bf8
Jonatha Targino <j.oliveiratargino@hotmail.com>
2026-01-21 19:19:31 -0300

    feat: improve store availability tests

--- iffood-api/src/modules/store/store-availability/tests/store-availability-service.test.ts
+++ iffood-api/src/modules/store/store-availability/tests/store-availability-service.test.ts
@@ -13,14 +13,48 @@ describe('StoreAvailability Service', () => {
       providers: [
         StoreAvailabilityService,
         { provide: StoreAvailabilityRepository, useValue: {} },
-        { provide: DataSource, useValue: {} },
+        {
+          provide: DataSource,
+          useValue: {
+            transaction: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     storeAvailabilityService = moduleRef.get(StoreAvailabilityService);
   });
 
-  it('(updateFullStoreAvailability) it should check duplicated weekdays', async () => {
+  it('(updateFullStoreAvailability) it should not throw error when receive valid availabilities', async () => {
+    const inputAvailabilities: ServiceUpdateStoreAvailabilityDto['availabilities'] =
+      [
+        {
+          end: '18:00',
+          start: '09:00',
+          weekday: 1,
+        },
+        {
+          end: '18:00',
+          start: '11:00',
+          weekday: 2,
+        },
+        {
+          end: '18:00',
+          start: '10:00',
+          weekday: 3,
+        },
+      ];
+
+    await expect(async () => {
+      await storeAvailabilityService.updateFullStoreAvailability({
+        storeId: 'some-store-id',
+        userId: 'some-user-id',
+        availabilities: inputAvailabilities,
+      });
+    }).resolves.toEqual(undefined);
+  });
+
+  it('(updateFullStoreAvailability) it should throw error when receive duplicated weekdays', async () => {
     const inputAvailabilities: ServiceUpdateStoreAvailabilityDto['availabilities'] =
       [
         {
@@ -37,6 +71,30 @@ describe('StoreAvailability Service', () => {
           end: '18:00',
           start: '09:00',
           weekday: 1,
+        },
+      ];
+
+    await expect(async () => {
+      await storeAvailabilityService.updateFullStoreAvailability({
+        storeId: 'some-store-id',
+        userId: 'some-user-id',
+        availabilities: inputAvailabilities,
+      });
+    }).rejects.toThrow(BadRequestException);
+  });
+
+  it('(updateFullStoreAvailability) it should throw error when receive a week day with start time greater than end time', async () => {
+    const inputAvailabilities: ServiceUpdateStoreAvailabilityDto['availabilities'] =
+      [
+        {
+          end: '08:00',
+          start: '09:00',
+          weekday: 1,
+        },
+        {
+          end: '18:00',
+          start: '09:00',
+          weekday: 2,
         },
       ];
 
