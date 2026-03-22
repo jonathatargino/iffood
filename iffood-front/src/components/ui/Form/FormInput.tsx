f3755e67b5f4925ebf437dd1a9655fa89b7c185f
Jonatha Targino <j.oliveiratargino@hotmail.com>
2026-03-22 09:41:05 -0300

    fix: front build

--- iffood-front/src/components/ui/Form/FormInput.tsx
+++ iffood-front/src/components/ui/Form/FormInput.tsx
@@ -1,6 +1,5 @@
 import type { HTMLInputTypeAttribute } from "react";
 import { useFormContext } from "react-hook-form";
-import { Button } from "../button";
 
 interface FormInputProps {
   label?: string;
